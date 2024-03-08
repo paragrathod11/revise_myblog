0dd81a6d8a95ee88069e20bb2dfb6725a9d64cc0
paragrathod11 <paragrathod11@gmail.com>
2024-03-08 11:41:57 +0530

    v1_default_Project_backup

---
+++ src/main/java/com/myblog/ReviseMyblogApplication.java
@@ -0,0 +1,13 @@
+package com.myblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviseMyblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviseMyblogApplication.class, args);
+	}
+
+}
