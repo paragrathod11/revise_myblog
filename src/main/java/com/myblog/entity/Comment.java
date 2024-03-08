9ddbc6c46e4b580f05f89b570d6a57790dadd1a7
paragrathod11 <paragrathod11@gmail.com>
2024-03-08 12:05:10 +0530

    v1_adding_Entity_Classes

---
+++ src/main/java/com/myblog/entity/Comment.java
@@ -0,0 +1,32 @@
+package com.myblog.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "revise_comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", unique = true)
+    private Post post;
+
+
+}
---
+++ src/main/java/com/myblog/entity/Post.java
@@ -0,0 +1,34 @@
+package com.myblog.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "revise_posts", uniqueConstraints = @UniqueConstraint(columnNames = {"title"}))
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, unique = true)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+    @Column(nullable = false)
+    private String description;
+
+    @OneToMany(mappedBy = "post",cascade =CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private Set<Comment> comment = new HashSet<>();
+
+
+}
