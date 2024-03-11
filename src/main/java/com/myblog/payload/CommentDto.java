b330c7cf6b37f1bf0d405d5d2f3c3df8cf47934e
paragrathod11 <paragrathod11@gmail.com>
2024-03-11 19:39:02 +0530

    v1_add_swagger_in_Payload/Dto_Classes

--- src/main/java/com/myblog/payload/CommentDto.java
+++ src/main/java/com/myblog/payload/CommentDto.java
@@ -1,5 +1,7 @@
 package com.myblog.payload;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -11,17 +13,23 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
+@ApiModel(description = "Comment Model Information.")
 public class CommentDto {
 
+    @ApiModelProperty(value = "The unique identifier of the comment")
     private long id;
     @NotEmpty
     @Size(min = 2, message = "Name should have at least 2 character.")
+    @ApiModelProperty(value = "Name of the commenter", example = "Abc Xyz")
     private String name;
     @NotEmpty
     @Email(message = "Email should have at least 12 characters.")
+    @ApiModelProperty(value = "Email of the commenter", example = "abc.xyz@email.com")
     private String email;
     @NotEmpty
     @Size(min = 2, message = "Body should have at least 2 characters.")
+    @ApiModelProperty(value = "Content/Body of the comment", example = "This is great post.")
     private String body;
 
 }
--- src/main/java/com/myblog/payload/PostDto.java
+++ src/main/java/com/myblog/payload/PostDto.java
@@ -1,7 +1,8 @@
 package com.myblog.payload;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,18 +13,26 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
+@ApiModel(description = "Post model information")
 public class PostDto {
 
+    @ApiModelProperty(value = "The unique identifier of the post")
     private long id;
     @NotEmpty
     @Size(min = 2, message = "Title should have at least 2 character.")
+    @ApiModelProperty(value = "The title of the post", example = "My Post Title")
     private String title;
     @NotEmpty
     @Size(min = 10, message = "Description should have at least 10 character.")
+    @ApiModelProperty(value = "The description of the post", example = "A detailed description of my post")
     private String description;
     @NotEmpty
     @Size(min = 5, message = "Content should have at least 5 character.")
+    @ApiModelProperty(value = "The content of the post", example = "This is the main content of my post")
     private String content;
 
+    @ApiModelProperty(value = "Set of comments associated with the post")
     private Set<CommentDto> comments;
+
 }
