7cc085eafb6911b6eed940e50a5df365eb4b470d
paragrathod11 <paragrathod11@gmail.com>
2024-03-13 09:31:14 +0530

    v1_add_Repository_Class

---
+++ src/main/java/com/myblog/repository/CommentRepository.java
@@ -0,0 +1,8 @@
+package com.myblog.repository;
+
+import com.myblog.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
