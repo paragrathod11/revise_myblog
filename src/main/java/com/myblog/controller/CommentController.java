1ded4de954357a5185bf8367429d83620816ea0a
paragrathod11 <paragrathod11@gmail.com>
2024-03-19 19:55:00 +0530

    v1_add_Swagger_in_Controllers

--- src/main/java/com/myblog/controller/CommentController.java
+++ src/main/java/com/myblog/controller/CommentController.java
@@ -2,6 +2,10 @@ package com.myblog.controller;
 
 import com.myblog.payload.CommentDto;
 import com.myblog.service.CommentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/post")
+@Api(value = "Comment Management System", description = "Operation for the Blog post's Comments management")
 public class CommentController {
 
     private CommentService commentService;
@@ -21,6 +26,11 @@ public class CommentController {
 
     // localhost:8080/api/post/1/comments
     @PostMapping("/{post_id}/comments")
+    @ApiOperation(value = "Create new Comment for particular post")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successfully created a new Comment"),
+            @ApiResponse(code = 400, message = "Bad Request, Validation error in the request body.")
+    })
     public ResponseEntity<CommentDto> createComment(@PathVariable("post_id") long post_id, @Valid @RequestBody CommentDto commentDto){
         return ResponseEntity.ok().body(commentService.createComment(post_id, commentDto));
     }
@@ -28,6 +38,11 @@ public class CommentController {
 
     // localhost:8080/api/post/allComments
     @GetMapping("/allComments")
+    @ApiOperation(value = "Retrieve all Comments")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieve all comments of all posts."),
+            @ApiResponse(code = 404, message = "Comments are not found.")
+    })
     public ResponseEntity<List<CommentDto>> getAllComments(){
         return
                 ResponseEntity.ok().body(commentService.getAllComments());
@@ -35,6 +50,11 @@ public class CommentController {
 
     // localhost:8080/api/post/1/comments
     @GetMapping("/{post_id}/comments")
+    @ApiOperation(value = "Retrieve all comments of particular post.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieve all comments of a post"),
+            @ApiResponse(code = 404, message = "Comments not found for a post.")
+    })
     public ResponseEntity<List<CommentDto>> getAllCommentByPostId(@PathVariable("post_id") long post_id){
         return ResponseEntity.ok().body(commentService.getAllCommentsByPostId(post_id));
     }
@@ -42,6 +62,11 @@ public class CommentController {
 
     // localhost:8080/api/post/1/comments/2
     @GetMapping("/{post_id}/comments/{comment_id}")
+    @ApiOperation(value = "Retrieve particular comment of a particular post.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Successfully found a particular comment."),
+            @ApiResponse(code = 404, message = "Particular comment not found.")
+    })
     public ResponseEntity<CommentDto> getPostCommentByCommentId(
             @PathVariable("post_id") long post_id,
             @PathVariable("comment_id") long comment_id
@@ -52,6 +77,12 @@ public class CommentController {
 
     // localhost:8080/api/post1/comments/2
     @PutMapping("/{post_id}/comments/{comment_id}")
+    @ApiOperation(value = "Update particular comment of a post")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Successfully updated the comment of the post."),
+            @ApiResponse(code = 400, message = "Bad Request. Validation error in the request body."),
+            @ApiResponse(code = 404, message = "Comment not found.")
+    })
     public ResponseEntity<CommentDto> editPostCommentByCommentId(
             @PathVariable("post_id") long post_id,
             @PathVariable("comment_id") long comment_id,
@@ -62,6 +93,11 @@ public class CommentController {
     }
 
     @DeleteMapping("{post_id}/comments/{comment_id}")
+    @ApiOperation(value = "Delete particular comment by comment id.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully deleted the comment."),
+            @ApiResponse(code = 404, message = "Comment not found.")
+    })
     public ResponseEntity<String> deletePostCommentByCommentId(
             @PathVariable("post_id") long post_id,
             @PathVariable("comment_id") long comment_id
@@ -69,9 +105,6 @@ public class CommentController {
         commentService.deletePostCommentByCommentId(post_id, comment_id);
         return ResponseEntity.accepted().body("Comment Deleted Successfully.");
     }
-
-
-
 
 
 }
--- src/main/java/com/myblog/controller/PostController.java
+++ src/main/java/com/myblog/controller/PostController.java
@@ -3,6 +3,7 @@ package com.myblog.controller;
 import com.myblog.payload.PostDto;
 import com.myblog.payload.PostResponse;
 import com.myblog.service.PostService;
+import io.swagger.annotations.*;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +11,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/post")
+@Api(value = "Post Management system", description = "Operation for the Blog post management")
 public class PostController {
 
-    private PostService postService;
+    private final PostService postService;
     public PostController(PostService postService) {
         this.postService = postService;
     }
@@ -20,6 +22,11 @@ public class PostController {
     // Create a Blog Post
     // localhost:8080/api/post
     @PostMapping
+    @ApiOperation(value = "Create a new Blog Post.")
+    @ApiResponses(value ={
+        @ApiResponse(code = 201, message = "Successfully created a new Blog Post."),
+        @ApiResponse(code = 400, message = "Bad Request. Validation error in the request body.")
+    })
     public PostDto savePost(@Valid @RequestBody PostDto postDto){
         return postService.createPost(postDto);
     }
@@ -28,6 +35,11 @@ public class PostController {
     // Get/Find Post data by post id
     // localhost:8080/api/post/1
     @GetMapping("/{id}")
+    @ApiOperation(value = "Get Blog post by 'post id'.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved the blog post."),
+            @ApiResponse(code = 404, message = "Blog post not found.")
+    })
     public PostDto getPostById(@PathVariable long id){
         return postService.getPostById(id);
     }
@@ -44,11 +56,21 @@ public class PostController {
 
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "Update a blog post by its id.", response = PostDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully updated the blog post"),
+            @ApiResponse(code = 404, message = "Blog post not found")
+    })
     public PostDto updatePostById(@PathVariable long id, @Valid @RequestBody PostDto postDto){
         return postService.updatePostById(id, postDto);
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Delete a blog post by its id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully deleted the blog post"),
+            @ApiResponse(code = 404, message = "Blog post not found")
+    })
     public String deletePostById(@PathVariable long id){
         postService.deletePostById(id);
         return "Deleted Successfully.";
